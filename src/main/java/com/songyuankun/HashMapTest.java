a357ef75c7707f8464f2d53350a7bd7566b82390
songyuankun <songyuankun@huohua.cn>
2021-08-30 22:13:55 +0800

    hashMap

--- src/main/java/com/songyuankun/HashMapTest.java
+++ src/main/java/com/songyuankun/HashMapTest.java
@@ -5,10 +5,10 @@ import java.util.HashMap;
 public class HashMapTest {
 
     public static void main(String[] args) {
-        HashMap<String, Integer> integerIntegerHashMap = new HashMap<>(2);
+        HashMap<String, Integer> hashMap = new HashMap<>(2);
 
         for (int i = 0; i < 3000; i++) {
-            integerIntegerHashMap.put(String.valueOf(i), i);
+            hashMap.put(String.valueOf(i), i);
         }
 
     }
