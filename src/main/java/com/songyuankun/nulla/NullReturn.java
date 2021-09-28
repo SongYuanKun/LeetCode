5b3ab7ce0468fb39c6409c015747e6939d65fbeb
songyuankun <songyuankun@huohua.cn>
2021-09-28 20:03:24 +0800

    nullreturn

---
+++ src/main/java/com/songyuankun/nulla/NullReturn.java
@@ -0,0 +1,10 @@
+package com.songyuankun.nulla;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.LOCAL_VARIABLE)
+@Retention(RetentionPolicy.SOURCE)
+@Documented
+public @interface NullReturn {
+
+}
